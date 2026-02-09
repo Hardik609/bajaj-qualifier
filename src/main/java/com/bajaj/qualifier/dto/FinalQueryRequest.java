9531506899b173067491c7d8b56a202cc74075e0
Hardik609 <jontybansla2002@gmail.com>
2026-02-09 15:07:52 +0530

    Delete src directory

--- src/main/java/com/bajaj/qualifier/dto/FinalQueryRequest.java
+++
@@ -1,9 +0,0 @@
-package com.bajaj.qualifier.dto;
-
-public class FinalQueryRequest {
-    private String finalQuery;
-
-    public FinalQueryRequest(String finalQuery) {
-        this.finalQuery = finalQuery;
-    }
-}
